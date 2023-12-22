536f33d985d2b224e590cffcbe35576e63ea7413
SUN <63995914+sunkim99@users.noreply.github.com>
2023-12-22 19:28:33 +0900

    231222

---
+++ 11655.py
@@ -0,0 +1,13 @@
+word = input()
+
+ans = ''
+
+for i in word:
+    if i.islower():
+        print(chr(97 + (ord(i) + 13 - 97) % 26), end='')
+
+    elif i.isupper():
+        print(chr(65 + (ord(i) + 13 - 65) % 26), end='')
+
+    else:
+        print(i,end='')
